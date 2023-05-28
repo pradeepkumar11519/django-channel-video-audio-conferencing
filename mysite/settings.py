7c262a086466d57ecb462e9f45369675caa5d18a
Pradeep_Kumar_Rebbavarapu <rpkiit2022@gmail.com>
2023-05-28 23:02:02 +0530

    i

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,13 +77,15 @@ ASGI_APPLICATION = "mysite.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default":dj_database_url.config(default = 'postgres://root:jo8OcmlZLzxM0x8SI9LyK5CAKfvppwje@dpg-chpos6fdvk4goevgv70g-a.oregon-postgres.render.com/testvideoconferencing',ssl_require=True)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
